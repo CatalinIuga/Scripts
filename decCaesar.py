9f6a082dc56543b67b10cfddb209c1ebac6ddaeb
CatalinIuga <cataliniuga34@gmail.com>
2022-07-22 23:18:31 +0300

    small improvements to decCaesar

--- decCaesar.py
+++ decCaesar.py
@@ -11,15 +11,30 @@ def is_valid(decoded):
     check = SpellChecker('en_US')
     check.set_text(decoded)
     err_no = [eror for eror in check]
-    return len(max_pct) - len(err_no)
+    return (len(max_pct) - len(err_no))/len(max_pct)*100
 
 
-def dict_sort(x):
-    return dict(sorted(x.items(), key=lambda item: item[1]))
+def check_for_spaces(l1, l2):
+    for i in range(len(l1)):
+        if i:
+            print(l)
 
 
-results = {}
-to_dec = sys.argv[1].lower()
+if len(sys.argv)<=1:
+    print('Enter a string to decript ')
+    inp = input()
+    if len(inp)==0:
+        print('Emplty string not accepted!')
+        exit()
+elif len(sys.argv)>=3:
+    print('Too many arguments. If the string contains whitespaces make sure to use \'\' or ""')
+    exit()
+else:
+    inp = sys.argv[1]
+
+result_string = []
+result_succes_r8 = []
+to_dec = inp.lower()
 alp = ascii_lowercase
 for i in range(0, 26):
     aux = alp[i % 26:] + alp[:i % 26]
@@ -29,7 +44,15 @@ for i in range(0, 26):
             dec += l
         else:
             dec += alp[aux.find(l)]
-    results[dec] = is_valid(dec)
-results = dict_sort(results)
-for d in results.keys():
-    print(d, results[d])
+    result_string.append(dec)
+    result_succes_r8.append(is_valid(dec))
+
+if(max(result_succes_r8) == 0):
+    print('The resulted string dosent look readable. Perhaps the \' \' was swapped for another letter.')
+    print('Possible solutions are:')
+else:
+    print('Found valid english words in the following solutions:')
+print('<------------------------------------->\nWord\tValidation percentage\n<------------------------------------->')
+for i in range(len(result_string)):
+    best = result_succes_r8.index(max(result_succes_r8))
+    print(result_string.pop(best), result_succes_r8.pop(best),sep='\t\t')
